7053be3ba83d281cd244cad4122939acb1fa8cf6
Jesper Hossy <jhossy@hotmail.com>
2021-08-14 21:01:44 +0200

    added vue router

--- src/App.vue
+++ src/App.vue
@@ -1,109 +1,31 @@
 <template>
   <div class="container">
       <Header @toggle-add-task="toggleAddTask" title="Task Tracker" :showAddTask="showAddTask" />
-      <div v-show="showAddTask">
-        <AddTask @add-task="addTask" />
-      </div>      
-      <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />      
+     
+      <router-view :showAddTask="showAddTask"></router-view>
+      <Footer />
     </div>
 </template>
 
 <script>
 import Header from './components/Header'
-import Tasks from './components/Tasks'
-import AddTask from './components/AddTask'
+import Footer from './components/Footer'
 
 export default {
   name: 'App',
   components: {
     Header,
-    Tasks,
-    AddTask
+    Footer
   },
   data() {
     return {
-      tasks: [],
       showAddTask: false
     }
   },
   methods: {
     toggleAddTask() {
       this.showAddTask = !this.showAddTask;
-    },
-    async addTask(task) {
-      const res = await fetch('api/tasks', {
-        method: 'POST',
-        headers: {
-          'content-type': 'application/json',
-        },
-        body: JSON.stringify(task),
-      });
-
-    const data = await res.json();
-
-      this.tasks = [...this.tasks, data]
-    },
-    async deleteTask(id) {
-      if(confirm('Are you sure?')){
-        const res = await fetch(`api/tasks/${id}`,{
-          method: 'DELETE'
-        });
-
-        res.status === 200 ?
-          (this.tasks = this.tasks.filter((task) => task.id !== id)) :
-          alert('Error deleting task')
-      }      
-    },
-    async toggleReminder(id){
-      const taskToToggle = await this.fetchTask(id);
-
-      const updTask = {...taskToToggle, reminder: !taskToToggle.reminder}
-
-      const res = await fetch(`api/tasks/${id}`, {
-        method: 'PUT',
-        headers: {
-          'content-type': 'application/json'
-        },
-        body: JSON.stringify(updTask)
-      });
-
-      const data = await res.json();
-
-      this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: data.reminder} : task)
-    },
-    async fetchTasks() {
-      const res = await fetch('api/tasks');
-      const data = await res.json();
-      return data;
-    },
-    async fetchTask(id) {
-      const res = await fetch(`api/tasks/${id}`);
-      const data = await res.json();
-      return data;
-    }  
-  },
-  async created() {
-    this.tasks = await this.fetchTasks();
-    // this.tasks = [
-    //   {
-    //     id: 1,
-    //     text: 'Doctors Appointment',
-    //     day: 'March 1st at 2:30pm',
-    //     reminder: true
-    //   },
-    //   {
-    //     id: 2,
-    //     text: 'Meeting at School',
-    //     day: 'March 3rd at 1:30pm',
-    //     reminder: true
-    //   },
-    //   {
-    //     id: 3,
-    //     text: 'Food shopping',
-    //     day: 'March 3rd at 11:00am',
-    //     reminder: false
-    //   }
-    // ]
+    }    
   }
 }
 </script>
---
+++ src/views/Home.vue
@@ -0,0 +1,103 @@
+<template>
+     <div v-show="showAddTask">
+        <AddTask @add-task="addTask" />
+      </div>      
+      <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />            
+</template>
+
+<script>
+    import Tasks from '../components/Tasks'
+    import AddTask from '../components/AddTask'
+
+    export default {
+        name: 'Home',
+        props: {
+            showAddTask: Boolean,
+        },
+        components: {
+            Tasks,
+            AddTask
+        },
+        data() {
+            return {
+                tasks: []
+            }            
+        },
+        methods: {
+            async addTask(task) {
+                const res = await fetch('api/tasks', {
+                    method: 'POST',
+                    headers: {
+                    'content-type': 'application/json',
+                    },
+                    body: JSON.stringify(task),
+                });
+
+                const data = await res.json();
+
+                this.tasks = [...this.tasks, data]
+            },
+            async deleteTask(id) {
+                if(confirm('Are you sure?')){
+                    const res = await fetch(`api/tasks/${id}`,{
+                    method: 'DELETE'
+                    });
+
+                    res.status === 200 ?
+                    (this.tasks = this.tasks.filter((task) => task.id !== id)) :
+                    alert('Error deleting task')
+                }      
+            },
+            async toggleReminder(id){
+                const taskToToggle = await this.fetchTask(id);
+
+                const updTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+
+                const res = await fetch(`api/tasks/${id}`, {
+                    method: 'PUT',
+                    headers: {
+                    'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(updTask)
+                });
+
+                const data = await res.json();
+
+                this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: data.reminder} : task)
+            },
+            async fetchTasks() {
+                const res = await fetch('api/tasks');
+                const data = await res.json();
+            return data;
+            },
+            async fetchTask(id) {
+                const res = await fetch(`api/tasks/${id}`);
+                const data = await res.json();
+            return data;
+            }  
+        },
+        async created() {
+            this.tasks = await this.fetchTasks();
+            // this.tasks = [
+            //   {
+            //     id: 1,
+            //     text: 'Doctors Appointment',
+            //     day: 'March 1st at 2:30pm',
+            //     reminder: true
+            //   },
+            //   {
+            //     id: 2,
+            //     text: 'Meeting at School',
+            //     day: 'March 3rd at 1:30pm',
+            //     reminder: true
+            //   },
+            //   {
+            //     id: 3,
+            //     text: 'Food shopping',
+            //     day: 'March 3rd at 11:00am',
+            //     reminder: false
+            //   }
+            // ]
+        }
+    }
+</script>
